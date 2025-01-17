9779581a479ea63792dd551d988cc63804658d8a
amrutap2004 <amrutapatil2004@gmail.com>
2025-01-17 12:11:59 +0530

    added enble dark mode

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -27,10 +27,10 @@ export default function TextForm(props) {
 
   return (
     <>
-        <div className='container my-3'>
+        <div className='container my-3' style= {{color : props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnchange} id="mytextarea" rows="8"></textarea> 
+            <textarea className="form-control" value={text} style= {{backgroundColor : props.mode==='dark'?'#06244f':'white' , color : props.mode==='dark'?'white':'black'}}onChange={handleOnchange} id="mytextarea" rows="8"></textarea> 
         </div>
         <button type="submit" className="btn btn-primary mx-3" onClick={handleUpclick}>Convert to Uppercase</button>
         <button type="submit" className="btn btn-primary mx-3" onClick={handleLoclick}>Convert to Lowercase</button>
@@ -39,12 +39,12 @@ export default function TextForm(props) {
 
         </div>
 
-        <div className="container">
+        <div className="container" style= {{color : props.mode==='dark'?'white':'black'}}>
             <h2>Text Summary</h2>
             <p> {text.split(" ").length} words and {text.length} characters</p>
             <p> {0.008 * text.split(" ").length} Minutes to read</p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text :"Enter Text in above box to preview it here."}</p>
 
         </div>
     </>
