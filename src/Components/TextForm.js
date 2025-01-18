08b2c644285a6ed026ef70ca666fb2568439e032
amrutap2004 <amrutapatil2004@gmail.com>
2025-01-18 16:38:51 +0530

    Your commit message

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -35,19 +35,19 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} style= {{backgroundColor : props.mode==='dark'?'#06244f':'white' , color : props.mode==='dark'?'white':'black'}}onChange={handleOnchange} id="mytextarea" rows="8"></textarea> 
         </div>
-        <button type="submit" className="btn btn-primary mx-3" onClick={handleUpclick}>Convert to Uppercase</button>
-        <button type="submit" className="btn btn-primary mx-3" onClick={handleLoclick}>Convert to Lowercase</button>
-        <button type="submit" className="btn btn-primary mx-3" onClick={handleClearclick}>Clear text</button>
+        <button type="submit" disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleUpclick}>Convert to Uppercase</button>
+        <button type="submit" disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleLoclick}>Convert to Lowercase</button>
+        <button type="submit" disabled={text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleClearclick}>Clear text</button>
         
 
         </div>
 
         <div className="container" style= {{color : props.mode==='dark'?'white':'black'}}>
             <h2>Text Summary</h2>
-            <p> {text.split(" ").length} words and {text.length} characters</p>
-            <p> {0.008 * text.split(" ").length} Minutes to read</p>
+            <p> {text.split(" ").filter((ele)=>{return ele.length !== 0}).length} words and {text.length} characters</p>
+            <p> {0.008 * text.split(" ").filter((ele)=>{return ele.length !== 0}).length} Minutes to read</p>
             <h3>Preview</h3>
-            <p>{text.length > 0 ? text :"Enter Text in above box to preview it here."}</p>
+            <p>{text.length > 0 ? text :"Nothing to Preview."}</p>
 
         </div>
     </>
